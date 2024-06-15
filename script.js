7f1497d0e231b7d70a478868cb01b23a5bc66def
Innisfree-11 <rogersdivyam@gmail.com>
2024-06-16 00:55:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let x = document.querySelectorAll(".buttons");
+let screen_text = document.querySelectorAll("p")[0];
+x.forEach(btn =>{
+    btn.addEventListener("click" , ()=>{
+console.log(btn.innerText , "was clicked");
+
+if(btn.innerText == "C"){
+let str = screen_text.innerText;
+if(str.length > 0){
+    str = str.substring(0, str.length - 1);
+}
+screen_text.innerText = str;
+
+}
+else if(btn.innerText == "AC"){
+screen_text.innerText = "";
+}
+else if(btn.innerText == "="){
+    screen_text.innerText = eval(screen_text.innerText);
+}
+else {
+    str = screen_text.innerText;
+    str = str + btn.innerText;
+    screen_text.innerText = str;
+}
+    })
+})
